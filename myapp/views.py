5a72909ca8f0c8153efc323375f1487804d4b8eb
mahruf60 <mahruf9060@gmail.com>
2025-01-23 22:44:33 +0600

    Saving link to Database

--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup 
+from .models import Link
 # Create your views here.
 
 def scrape(request):
-    page = requests.get('https://www.facebook.com/')
+    page = requests.get('https://www.google.com/')
     soup = BeautifulSoup(page.text,'html.parser')
-    link_address = []
     for link in soup.find_all('a'):
-        link_address.append(link.get('href'))
-    return render(request,'myapp/result.html',{'link_address':link_address})
+        link_address = link.get('href')
+        link_text = link.string
+        Link.objects.create(address=link_address,name=link_text) 
+
+    data = Link.objects.all()
+
+    return render(request,'myapp/result.html',{'data':data})
